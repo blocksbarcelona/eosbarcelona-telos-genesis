e24ef6178c941864c061e566c422d04b2dddc9cb
Josep Rosich <jrosich@gmail.com>
2018-10-17 16:36:37 +0200

    Add validate accounts script

---
+++ validate_accounts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# eosBarcelona - Josep Rosich - Updated: Oct 17th 2018
+# This script validates accounts on chain and outputs a CSV that can be compared with the balance loaded
+
+rm ./genesis_ok.csv
+rm ./genesis_error.csv
+
+for line in $(tail -n +2 tlos_genesis_snapshot_inject.csv); do
+    ACCOUNT=$(echo $line | tr "," "\n" | head -3 | tail -1)
+	ORG=$(echo $line | tr -d "\r" | head -1)
+	JSON=$(./teclos.sh get account $ACCOUNT -j)
+	NAME=$(echo $JSON | jq .total_resources.account_name | cut -d ' ' -f 1 | cut -d '"' -f 2)
+	NET_RES=$(echo $JSON | jq .total_resources.net_weight | cut -d ' ' -f 1 | cut -d '"' -f 2)
+	CPU_RES=$(echo $JSON | jq .total_resources.cpu_weight | cut -d ' ' -f 1 | cut -d '"' -f 2)
+	LIQ=$(echo $JSON | jq .core_liquid_balance | cut -d ' ' -f 1 | cut -d '"' -f 2)
+	TOTAL=$(echo "$NET_RES + $CPU_RES + $LIQ" | bc)
+
+  	if  [ $NAME ]; then
+		echo "$ORG,$TOTAL" >> genesis_ok.csv
+	else
+		echo "$line" >> genesis_error.csv
+	fi
+done
+
