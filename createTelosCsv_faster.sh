9d4ac67eeb5ab6989dd5ec13ad148eadf42a491f
Josep IPS <jrosich@ipssoft.com>
2018-10-22 11:25:21 +0200

    added python createTelosCSV_faster.sh

---
+++ createTelosCsv_faster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# eosBarcelona - Josep Rosich - Oct 21st 2018
+# Create telos csv with cpu/bandwith/liquid ready to inject.
+# CSV created using python for a faster creation
+
+python generatecsv.py > telos_snapshot.csv
