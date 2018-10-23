29a26b7e8a38dcd179df9b6dffa594c238ed3a90
Josep <josep@gmail.com>
2018-10-23 09:37:45 +0200

    renaming scripts
    	renamed:    pytest.py -> validate-accresbalance.py
    	deleted:    validate.py

--- validate-accresbalance.py
+++ validate-accresbalance.py
@@ -8,20 +8,21 @@ csv_f = csv.reader(f)
 SCRIPTSDIR = "/opt/telos_main/scripts"
 i=1
 for row in csv_f:
-        account = row[1]
+    account = row[1]
 	key = row[2]
 	total = row[3]
 	cpu = row[4]
 	bandwith = row [5]
 	liq = row[6]
 
-        output = subprocess.check_output(SCRIPTSDIR + "/teclos.sh get account " + account + " -j ", shell=True)
-        jsonacc  = json.loads(output)
+    output = subprocess.check_output(SCRIPTSDIR + "/teclos.sh get account " + account + " -j ", shell=True)
+    jsonacc  = json.loads(output)
 	acc_liquid,waste = jsonacc["core_liquid_balance"].split()
 
 	if (acc_liquid == liq):
 		acc_resources = jsonacc["total_resources"]
-		acc_cpu,waste = acc_resources["net_weight"].split()
+		acc_net,waste = acc_resources["net_weight"].split()
+		acc_cpu,waste = acc_resources["cpu_weight"].split()
 
 		if (cpu != acc_cpu):
 			print( 'error cpu: ' + cpu + '!=' + acc_cpu)
